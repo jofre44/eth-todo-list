68d5119fdbc6971eb7dec3af5051269d5e982873
Isabel-Cristina <jofre44@gmail.com>
2021-03-16 20:17:22 +0100

    Toggle  Function

--- src/app.js
+++ src/app.js
@@ -96,14 +96,19 @@ App = {
 
             $newTaskTemplate.show()
         }
-
-
     },
 
     createTask: async () => {
         App.setLoading(true)
         const content = $('#newTask').val()
         await App.todoList.createTask(content)
+        window.location.reload()
+    },
+
+    toggleCompleted: async (event) => {
+        App.setLoading(true)
+        const taskId = event.target.name
+        await App.todoList.toggleCompleted(taskId)
         window.location.reload()
     },
 
